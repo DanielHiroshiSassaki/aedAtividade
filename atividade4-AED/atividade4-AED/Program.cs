88ff00dfdef038181d4543d42e50d25ef599db37
Daniel Hiroshi Sassaki Resende <103213246+DanielHiroshiSassaki@users.noreply.github.com>
2023-03-17 00:18:33 -0300

    add files

---
+++ atividade4-AED/atividade4-AED/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atividade4_AED
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            triangulo p1 = new triangulo();
+            Console.WriteLine("Triangulo");
+            Console.WriteLine("Digite o valor de A: ");
+            p1.setA(float.Parse(Console.ReadLine()));
+            Console.WriteLine("O valor de A é: " + p1.getA());
+            Console.WriteLine("\n\nDigite o valor de B: ");
+            p1.setB(float.Parse(Console.ReadLine()));
+            Console.WriteLine("O valor de B é: " + p1.getB());
+            Console.WriteLine("\n\nDigite o valor de C: ");
+            p1.setC(float.Parse(Console.ReadLine()));
+            Console.WriteLine("O valor de C é: " + p1.getC() + "\n\n");
+
+            p1.imprimirDados();
+        }
+    }
+}
---
+++ atividade4-AED/atividade4-AED/triangulo.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atividade4_AED
+{
+    internal class triangulo
+    {
+        private float a;
+        private float b;
+        private float c;
+
+
+        public triangulo() { }
+
+        public triangulo(float a, float b, float c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public void setA(float a)
+        {
+            this.a = a;
+        }
+        public float getA()
+        {
+            return this.a;
+        }
+
+        public void setB(float b)
+        {
+            this.b = b;
+        }
+        public float getB()
+        {
+            return this.b;
+        }
+        public void setC(float c)
+        {
+            this.c = c;
+        }
+        public float getC()
+        {
+            return this.c;
+        }
+
+        public void imprimirDados()
+        {
+            if (a >= b + c)
+            {
+                Console.WriteLine("Não forma um triangulo");
+            }
+
+            else if (a * a == ((b * b) + (c * c)))
+            {
+                Console.WriteLine("Triangulo Retaingulo");
+            }
+
+            else if (a * a > ((b * b) + (c * c)))
+            {
+                Console.WriteLine("Triangulo Obtusangulo");
+            }
+            else if ((a == b) && (b == c))
+            {
+                Console.WriteLine("Triangulo equilatero");
+            }
+            else if (a * a < ((b * b) + (c * c)))
+            {
+                Console.WriteLine("Triangulo Acutangulo");
+            }
+            else
+            {
+                Console.WriteLine("Triangulo isosceles");
+            }
+        }
+    }
+}
