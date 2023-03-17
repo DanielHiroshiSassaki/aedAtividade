62fa9f46449744054d50e2f8304757933a357107
Daniel Hiroshi Sassaki Resende <103213246+DanielHiroshiSassaki@users.noreply.github.com>
2023-03-17 00:17:08 -0300

    add files

---
+++ atividade3-Algoritmos/atividade3-Algoritmos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atividade3_Algoritmos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            banco p1 = new banco();
+            Console.WriteLine("Digite o Nome: ");
+            p1.setNome(Console.ReadLine());
+            Console.WriteLine("Digite o Salario: ");
+            p1.setSalario(double.Parse(Console.ReadLine()));
+            Console.WriteLine("Digite o total das vendas");
+            p1.setVendas(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Nome: " + p1.getNome());
+            Console.WriteLine("Salario: " + p1.getSalario());
+            Console.WriteLine("Vendas: " + p1.getVendas());
+            Console.WriteLine("Total: " + p1.getSalario() + (p1.getVendas() * 0.15));
+        }
+    }
+}
---
+++ atividade3-Algoritmos/atividade3-Algoritmos/banco.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atividade3_Algoritmos
+{
+    internal class banco
+    {
+        public string nome;
+        public double salario;
+        public double vendas;
+
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public string getNome()
+        {
+            return this.nome;
+        }
+
+        public void setSalario(double salario)
+        {
+            this.salario = salario;
+        }
+
+        public double getSalario()
+        {
+            return this.salario;
+        }
+
+        public void setVendas(double vendas)
+        {
+            this.vendas = vendas;
+        }
+
+        public double getVendas()
+        {
+            return this.salario = salario;
+        }       
+    }
+}
